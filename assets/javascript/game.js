a11ce3af5add7d86fb400a50475eb6f197a40837
aaron <aaronbushman@gmail.com>
2019-06-03 18:33:35 -0400

    initial js, add names var, random name selection

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var musicians = [
+"hank mobley",
+"john coltrane",
+"thelonius monk",
+"lee morgan",
+"sun ra",
+"ornette coleman",
+"don cherry",
+"pharoah sanders",
+"alice coltrane",
+];
+
+var musician = musicians[Math.floor(Math.random() * musicians.length)];
+
+for (var i = 0; i < musician.length; i++) {
+    nameSelection[i] = "_";
+}
+
+console.log(name)
